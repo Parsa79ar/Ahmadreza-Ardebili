c8869ae3a1987781d2c3f285b7a24550de3f4829
Parsa79ar <parsa.ardebili@gmail.com>
2024-03-18 00:56:07 +0330

    Fix github activity bug

--- main.js
+++ main.js
@@ -7,37 +7,37 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 })
 
 
-/* Vanilla RSS - https://github.com/sdepold/vanilla-rss */
+// /* Vanilla RSS - https://github.com/sdepold/vanilla-rss */
 
-	const rss = new RSS(
-	    document.querySelector("#rss-feeds"),
-	    //Change this to your own rss feeds
-        "https://feeds.feedburner.com/TechCrunch/startups",
-	    {
-		     // how many entries do you want?
-		    // default: 4
-		    // valid values: any integer
-		    limit: 3,
+// 	const rss = new RSS(
+// 	    document.querySelector("#rss-feeds"),
+// 	    //Change this to your own rss feeds
+//         "https://feeds.feedburner.com/TechCrunch/startups",
+// 	    {
+// 		     // how many entries do you want?
+// 		    // default: 4
+// 		    // valid values: any integer
+// 		    limit: 3,
 		    
 		    
-		    // will request the API via https
-			// default: false
-			// valid values: false, true
-			ssl: true,
+// 		    // will request the API via https
+// 			// default: false
+// 			// valid values: false, true
+// 			ssl: true,
 		  
-			 // outer template for the html transformation
-			// default: "<ul>{entries}</ul>"
-			// valid values: any string
-			layoutTemplate: "<div class='items'>{entries}</div>",
+// 			 // outer template for the html transformation
+// 			// default: "<ul>{entries}</ul>"
+// 			// valid values: any string
+// 			layoutTemplate: "<div class='items'>{entries}</div>",
 		
-			// inner template for each entry
-			// default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
-			// valid values: any string
-			entryTemplate: '<div class="item"><h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fas fa-external-link-alt"></i>Read more</a></div></div>',
+// 			// inner template for each entry
+// 			// default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
+// 			// valid values: any string
+// 			entryTemplate: '<div class="item"><h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fas fa-external-link-alt"></i>Read more</a></div></div>',
 		    
-	    }
-	);
-	rss.render();
+// 	    }
+// 	);
+// 	rss.render();
 
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
